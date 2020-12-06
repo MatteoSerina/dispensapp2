845821f40e1f5c86c21cd7b9191c187673059157
Matteo Serina <matteo.serina89@gmail.com>
2020-12-06 22:37:24 +0100

    Added ImageURL from Google

--- backend/controllers/catalog.js
+++ backend/controllers/catalog.js
@@ -1,17 +1,38 @@
 const Template = require('../models/template');
-const fs = require('fs');
+const imageSearch = require('image-search-google');
+const secrets = require('../secrets.ignore');
+
+const client = new imageSearch(secrets.googleCx, secrets.googleApiKey);
+const options = { num: 1, searchType: 'image', };
+
+async function addImageUrl(template) {
+    const images = await client.search(template.barcode, options);
+    template.imageUrl = images[0].url;
+    return template;
+}
 
 exports.createTemplate = (req, res, next) => {
-    const template = new Template({
-        barcode: req.body.barcode.toLowerCase(),
-        category: req.body.category.toLowerCase(),
-        itemsPerPackage: req.body.itemsPerPackage
-    });
-    template.save().then(
-        () => {
-            res.status(201).json({
-                message: 'Template created successfully!'
+    client.search(req.body.barcode, options).then(
+        (images) => {
+            const template = new Template({
+                barcode: req.body.barcode.toLowerCase(),
+                category: req.body.category.toLowerCase(),
+                itemsPerPackage: req.body.itemsPerPackage,
+                imageUrl: images[0].url
             });
+            template.save().then(
+                () => {
+                    res.status(201).json({
+                        message: 'Template created successfully!'
+                    });
+                }
+            ).catch(
+                (error) => {
+                    res.status(400).json({
+                        error: error
+                    });
+                }
+            );
         }
     ).catch(
         (error) => {
@@ -46,17 +67,26 @@ exports.updateTemplate = (req, res, next) => {
                 category: req.body.category.toLowerCase(),
                 itemsPerPackage: req.body.itemsPerPackage
             });
-            Template.updateOne({ _id: currentTemplate.id }, template).then(
-                (result) => {
-                    if (result.n > 0) {
-                        res.status(201).json({
-                            message: 'Template updated successfully'
-                        })
-                    } else {
-                        res.status(404).json({
-                            message: 'Template not found'
-                        })
-                    }
+            addImageUrl(template).then(
+                (template) => {
+                    console.log(template);
+                    Template.updateOne({ _id: template._id }, template).then(
+                        (result) => {
+                            if (result.n > 0) {
+                                res.status(201).json({
+                                    message: 'Template updated successfully'
+                                })
+                            } else {
+                                res.status(404).json({
+                                    message: 'Template not found'
+                                })
+                            }
+                        }
+                    ).catch(
+                        (error) => {
+                            res.status(400).json({ error: error })
+                        }
+                    )
                 }
             ).catch(
                 (error) => {
@@ -94,8 +124,8 @@ exports.deleteTemplate = (req, res, next) => {
         })
 }
 
-exports.getAllTemplates = (req, res, next) => {    
-    Template.find().sort({category: 1}).then(
+exports.getAllTemplates = (req, res, next) => {
+    Template.find().sort({ category: 1 }).then(
         (templates) => {
             res.status(200).json(templates);
         }
--- backend/models/template.js
+++ backend/models/template.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const templateSchema = new mongoose.Schema({
     barcode: { type: String, required: true },
     category: { type: String, required: true },
-    itemsPerPackage: { type: Number, required: true, default: 1 }
+    itemsPerPackage: { type: Number, required: true, default: 1 },
+    imageUrl:{ type: String, required: false }
 })
 
 module.exports = mongoose.model('Template', templateSchema);
