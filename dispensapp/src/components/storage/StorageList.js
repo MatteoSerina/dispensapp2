ff57cd86260ab656071147311a317f013bba8a9c
Matteo Serina <matteo.serina89@gmail.com>
2020-12-24 12:41:04 +0100

    Fixed app bar and category filter

--- dispensapp/src/components/storage/StorageList.js
+++ dispensapp/src/components/storage/StorageList.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import Paper from '@material-ui/core/Paper';
 import StorageCard from './StorageCard';
 
 const StorageList = (props) => {
@@ -10,17 +13,19 @@ const StorageList = (props) => {
   props.storage.forEach((good) => {
     if (!props.filter) {
       items.push(
-        <StorageCard good={good} onUpdate={handleUpdate}/>
+        <StorageCard good={good} onUpdate={handleUpdate} />
       )
     } else if (good.category.toLowerCase().indexOf(props.filter.toLowerCase()) !== -1) {
       items.push(
-        <StorageCard good={good} onUpdate={handleUpdate}/>
+        <StorageCard good={good} onUpdate={handleUpdate} />
       )
     }
   })
   return (
     <div>
-      {items}
+      <Paper style={{ maxHeight: '80vmax', overflow: 'auto' }}>      
+        {items}      
+      </Paper>
     </div>
   );
 }
