aaeefae58abed10f5e700dc86cd1fe442a191fb7
Matteo Serina <matteo.serina89@gmail.com>
2021-01-03 19:59:14 +0100

    added auth middleware to all the APIs

--- backend/routes/apiVersion.js
+++ backend/routes/apiVersion.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const auth = require('../middleware/auth');
 const apiVersionController = require('../controllers/apiVersion');
 
-router.get('', apiVersionController.getVersion);
+router.get('', auth, apiVersionController.getVersion);
 module.exports = router;
